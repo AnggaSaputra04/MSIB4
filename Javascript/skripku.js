fbc477a01651009dd8521d9a5b6a105a9f9e612c
AnggaSaputra04 <putrasangga043@gmail.com>
2023-03-19 16:14:27 +0700

    perbarui

--- Javascript/skripku.js
+++ Javascript/skripku.js
@@ -1,28 +1,3 @@
-/*function salam(){
-    //tangkap request form
-    let frm  = window.document.getElementById("formulir");
-    let mahasiswa = frm.nama.value;
-    /*
-    logic jika mahasiswa sudah input namanya ada salam
-    jika tidak input nama, maka ada pesan belum input nama 
-    */
-    /*if(mahasiswa != ""){
-        alert("Selamat Pagi" + mahasiswa);
-
-    }
-    else{
-        alert("Maaf anda belum input nama");
-    }
-    
-}
-
-function kosongin(){
-    //tangkap request form
-    let frm  = window.document.getElementById("formulir");
-    frm.nama.value = "";
-}*/
-
-
 function Login(){
     let user = document.getElementById("email").value;
     let pass = document.getElementById("password").value;
@@ -38,22 +13,4 @@ function Login(){
         alert(" Anda Gagal Login"); 
     }  
 }
-/*function Login(){
-    let user = document.getElementById("email").value;
-    let pass = document.getElementById("password").value;
-
-    if(user=="budi" && pass=="budi123"){
-        
-        alert("Selamat  Datang Budi, Anda Operator");
-       
-    } else{
-        alert("Anda Gagal Login");
-    }
-
-    
-    
-
-    
-}**/
-
 // alert("Selamat  Datang Budi, Anda Operator");
